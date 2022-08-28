3824df45e67aa01a42fa639d934f979f7767347b
Larissa Sthefany <110210572+Larissasthefany@users.noreply.github.com>
2022-08-27 22:52:55 -0300

    Exercício 003

--- ex001/script.js
+++ ex001/script.js
@@ -1,1 +1,4 @@
-console.log("Olá, Mundo!")
+alert("Minha primeira mensagem");
+
+window.confirm("Está gostando de Javascript?")
+window.prompt("Qual é o seu nome?")
---
+++ ex003/script.js
@@ -0,0 +1,5 @@
+var n1 = Number(prompt("Digite um número?"))
+var n2 = Number(prompt("Digite outro número?"))
+var soma = n1 + n2 
+
+alert(`A soma entre ${n1} e ${n2} é igual a ${soma}`)
