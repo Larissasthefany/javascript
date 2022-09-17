11a38edb35292882dd64eaaf1bca7ab3288b67fe
Larissa Sthefany <110210572+Larissasthefany@users.noreply.github.com>
2022-09-16 23:58:10 -0300

    Atualização

---
+++ exercicios/ex013/main.js
@@ -0,0 +1,5 @@
+var num = Number(prompt("Digite um numero"))
+
+var result = num * 2
+
+alert("O dobro do numero " +num+ " é " +result+ ".")
---
+++ exercicios/ex014/main.js
@@ -0,0 +1,7 @@
+// var metros =  parseFloat(prompt("Digite uma distância em metros:"))
+var metros = 185.72
+
+var distancia = metros * 1000
+var mil = metros * 10000
+
+alert(`A distância de ${metros} corresponde a ${distancia}cm  ${mil}mm`)
